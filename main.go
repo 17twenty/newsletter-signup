019990f0cebc2a938adce42a3db85627a1aac2b4
Nick Glynn <nick@spaceship.com.au>
2018-03-03 16:38:18 +1100

    Adding demos:

--- main.go
+++ main.go
@@ -12,10 +12,22 @@ import (
 func LandingHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "landing: %v\n", vars["category"])
+	fmt.Fprintf(w, "LandingHandler", vars)
 }
 
 func ThanksHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "thanks: %v\n", vars["category"])
+}
+
+func LatestHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "thanks: %v\n", vars["category"])
+}
+
+func ArchivesHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprintf(w, "thanks: %v\n", vars["category"])
@@ -25,6 +37,8 @@ func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", LandingHandler)
 	r.HandleFunc("/thankyou", ThanksHandler).Methods("POST")
+	r.HandleFunc("/latest", LatestHandler)
+	r.HandleFunc("/archives", ArchivesHandler)
 
 	// This will serve files under http://localhost:8000/static/<filename>
 	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("/static/"))))
