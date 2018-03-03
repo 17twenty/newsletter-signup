64d68c9fcc0cb43b08e006305fb651e202aff89e
Nick Glynn <nick@spaceship.com.au>
2018-03-03 16:58:10 +1100

    Start shit up

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 	"time"
+
+	_ "github.com/mattn/go-sqlite3"
 
 	"github.com/gorilla/mux"
 )
@@ -13,6 +17,16 @@ func LandingHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprintf(w, "LandingHandler", vars)
+}
+
+// ConfirmationHandler is <siteAddress>/confirm/TOKEN
+func ConfirmationHandler(w http.ResponseWriter, r *http.Request) {
+	// Confirm in DB and give them a confirmation.html
+	vars := mux.Vars(r)
+
+	log.Println(vars)
+	log.Println(vars["confirmationID"])
+	w.WriteHeader(http.StatusOK)
 }
 
 func ThanksHandler(w http.ResponseWriter, r *http.Request) {
@@ -33,9 +47,30 @@ func ArchivesHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "thanks: %v\n", vars["category"])
 }
 
+var (
+	siteAddress         = "https://testdomain.com"
+	localAddressAndPort = "127.0.0.1:8000"
+)
+
 func main() {
+
+	database, _ := sql.Open("sqlite3", "./content.db")
+	statement, _ := database.Prepare("CREATE TABLE IF NOT EXISTS people (id INTEGER PRIMARY KEY, firstname TEXT, lastname TEXT)")
+	statement.Exec()
+	statement, _ = database.Prepare("INSERT INTO people (firstname, lastname) VALUES (?, ?)")
+	statement.Exec("Demo", "Person")
+	rows, _ := database.Query("SELECT id, firstname, lastname FROM people")
+	var id int
+	var firstname string
+	var lastname string
+	for rows.Next() {
+		rows.Scan(&id, &firstname, &lastname)
+		fmt.Println(strconv.Itoa(id) + ": " + firstname + " " + lastname)
+	}
+
 	r := mux.NewRouter()
 	r.HandleFunc("/", LandingHandler)
+	r.HandleFunc("/confirm/{confirmationID}", ConfirmationHandler) // takes a confirmationID
 	r.HandleFunc("/thankyou", ThanksHandler).Methods("POST")
 	r.HandleFunc("/latest", LatestHandler)
 	r.HandleFunc("/archives", ArchivesHandler)
@@ -45,7 +80,7 @@ func main() {
 
 	srv := &http.Server{
 		Handler: r,
-		Addr:    "127.0.0.1:8000",
+		Addr:    localAddressAndPort,
 		// Good practice: enforce timeouts for servers you create!
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
--- readme.md
+++ readme.md
@@ -4,7 +4,15 @@ Easily customisable, single binary deploy email newsletter signup page.
 
 ## Getting Started
 
+Replace the following files:
+
+```
+static/favicon.ico
+static/logo.ico
+```
+
 ```bash
+$ go get ./...
 $ go get github.com/17twenty/newsletter-signup
 ...
 $ newsletter-signup
